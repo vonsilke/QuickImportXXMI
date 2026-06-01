e3e202dab5ec0a777c738078b23ec28a96579fc4
Gustav0_ <46067210+Seris0@users.noreply.github.com>
2026-05-31 23:30:18 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ import importlib
 bl_info = {
     "name": "XXMI Scripts & Quick Import",
     "author": "Gustav0, LeoTorreZ", 
-    "version": (3, 0, 7),
+    "version": (3, 0, 8),
     "blender": (3, 6, 2),
     "description": "Script Compilation",
     "category": "Object",
